1b814de85eea15f0d31f2adaa72491003584eed1
MariusMonkam <cloudmonde@yahoo.com>
2020-09-11 00:27:22 +0300

    add macro_nutriments

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("afriuniv.json");
+const router = jsonServer.router("./src/data/macro_nutriments.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
